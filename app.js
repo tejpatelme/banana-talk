fd16cbd806919ab610472bd9a8318fe71b154e6d
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2020-11-30 11:51:10 +0530

    add : script to test mock server

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 var btnTranslate = document.querySelector("#btnTranslate");
 var txtInput = document.querySelector("#txtInput");
 var txtOutput = document.querySelector("txtOutput");
+txtOutput.innerHTML = " ";
 
 serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
-function getTranslattionURL(text){
+function getTranslationURL(text){
     return serverURL + "?" + "text=" + text;
 }
 
 function btnClickHandler(){
     var inputText = txtInput.value;
-    fetch(getTranslattionURL(inputText))
-    .then(response => response.json)
-    .then(json => console.log(json))
+    console.log(inputText);
+    console.log(getTranslationURL(inputText))
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        translatedText = json.contents.translated;
+        txtOutput.innerHTML  = translatedText;
+    })
 }
 
 btnTranslate.addEventListener("click",btnClickHandler);
